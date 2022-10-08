3a3dd49bd24c09c58dd5024804101f00259a0437
NathanG-HEG <92545039+NathanG-HEG@users.noreply.github.com>
2022-10-08 23:21:59 +0300

    Done!

--- App.js
+++ App.js
@@ -1,20 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {StatusBar} from 'expo-status-bar';
+import {Button, StyleSheet, TextInput, View} from 'react-native';
+import * as Speech from 'expo-speech';
+import {useState} from "react";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+    const [text, setText] = useState('');
+
+    return (
+        <View style={styles.container}>
+            <TextInput style={{borderColor: 'black', borderWidth: 1, width: '90%', textAlign: 'center'}}
+                       onChangeText={text => setText(text)}
+                       value={text}
+                       placeholderTextColor={'grey'}
+                       placeholder={'Insert text to speech'}/>
+            <Button title={'Press to hear!'} onPress={() => {
+                Speech.speak(text)
+            }}/>
+            <StatusBar style="auto"/>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginVertical: '5%',
+    },
 });
